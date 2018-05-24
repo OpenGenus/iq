22763cea2926562f6b3e170293eb4536565383a3
Aditya Chatterjee <aditianhacker@gmail.com>
2018-05-25 00:04:26 +0530

    initial design

---
+++ code/assets/js/map/data/search-map.js
@@ -0,0 +1,145 @@
+const map = {  
+   "nodes":[  
+      {  
+         "text":"Search algorithms",
+         "url":"http://github.com/opengenus",
+         "category":"wiki",
+         "selector":"https://github.com",
+         "fx":-26.54482654584558,
+         "fy":-276.5486286463314
+      },
+      {  
+         "text":"binary search",
+         "selector":"",
+         "url":"",
+         "fx":-442.05492872462196,
+         "fy":-479.64907166675795
+      },
+      {  
+         "text":"exponential search",
+         "selector":"",
+         "url":"",
+         "fx":-331.7084090172766,
+         "fy":-28.781401207523203
+      },
+      {  
+         "text":"fibonacci search",
+         "selector":"",
+         "url":"",
+         "fx":-6.009030681073291,
+         "fy":-69.36991080636218
+      },
+      {  
+         "text":"fuzzy search",
+         "selector":"",
+         "url":"",
+         "fx":366.3712396634651,
+         "fy":-474.44894485229764
+      },
+      {  
+         "text":"interpolation search",
+         "selector":"",
+         "url":"",
+         "fx":319.531682218641,
+         "fy":-58.63586635094762
+      },
+      {  
+         "text":"jump search",
+         "selector":"",
+         "url":"",
+         "fx":-456.54482347549197,
+         "fy":-218.46618457601136
+      },
+      {  
+         "text":"linear search",
+         "selector":"",
+         "url":"",
+         "fx":421.9031511460596,
+         "fy":-245.45003383110492
+      },
+      {  
+         "text":"ternary search",
+         "selector":"",
+         "url":"",
+         "fx":-6.018578988319206,
+         "fy":-483.2794440423308
+      }
+   ],
+   "subnodes":[  
+
+   ],
+   "connections":[  
+      {  
+         "source":"binary search",
+         "target":"Search algorithms",
+         "curve":{  
+            "x":null,
+            "y":null
+         },
+         "index":0
+      },
+      {  
+         "source":"exponential search",
+         "target":"Search algorithms",
+         "curve":{  
+            "x":null,
+            "y":null
+         },
+         "index":1
+      },
+      {  
+         "source":"fibonacci search",
+         "target":"Search algorithms",
+         "curve":{  
+            "x":null,
+            "y":null
+         },
+         "index":2
+      },
+      {  
+         "source":"fuzzy search",
+         "target":"Search algorithms",
+         "curve":{  
+            "x":null,
+            "y":null
+         },
+         "index":3
+      },
+      {  
+         "source":"interpolation search",
+         "target":"Search algorithms",
+         "curve":{  
+            "x":null,
+            "y":null
+         },
+         "index":4
+      },
+      {  
+         "source":"jump search",
+         "target":"Search algorithms",
+         "curve":{  
+            "x":null,
+            "y":null
+         },
+         "index":5
+      },
+      {  
+         "source":"linear search",
+         "target":"Search algorithms",
+         "curve":{  
+            "x":null,
+            "y":null
+         },
+         "index":6
+      },
+      {  
+         "source":"ternary search",
+         "target":"Search algorithms",
+         "curve":{  
+            "x":null,
+            "y":null
+         },
+         "index":7
+      }
+   ]
+}
