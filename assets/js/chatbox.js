3bba11dc3ecb1cab0caa19826f0e9fa847f2e478
AchrafGarai <achrafgarai@hotmail.com>
2018-06-11 18:53:09 +0100

    Added an Icon.

--- assets/js/chatbox.js
+++ assets/js/chatbox.js
@@ -1,24 +1,29 @@
-var forum = document.getElementById("Discourse_Forum");
+
+
 
     // Discourse forum  popup and the live support feature goes here
 var discourse_msg_active=false;
+
          $(document).ready(function(){
 
              $('#Discourse_button').click(function(){
+
                if(discourse_msg_active==false){
-                 $('#Discourse_Forum').addClass("fadeMe");
-                 $('#Discourse_Forum').css("display","block");
+
+                $('#Discourse_Forum').slideDown(300);
                  discourse_msg_active=true;
+
                }else{
-                 $('#Discourse_Forum').css("display","none");
+                    $('#Discourse_Forum').slideUp(300);
+
                  discourse_msg_active=false;
 
                }
 
             });
+            // Controls for the close button for this popup
             $('#Discourse_close').click(function(){
-
-                $('#Discourse_Forum').css("display","none");
+                $('#Discourse_Forum').slideUp(300);
                 discourse_msg_active=false;
 
             });
