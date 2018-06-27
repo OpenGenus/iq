0eb03b202830db620e007f04a4febb21f1d3ef3c
AchrafGarai <achrafgarai@hotmail.com>
2018-06-27 16:20:52 +0100

    Added code embedding feature

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,39 +1,35 @@
- // tabbed content
-    // http://www.entheosweb.com/tutorials/css/tabs.asp
-    $(".tab_content").hide();
-    $(".tab_content:first").show();
 
-  /* if in tab mode */
-    $("ul.tabs li").click(function() {
-		
-      $(".tab_content").hide();
-      var activeTab = $(this).attr("rel"); 
-      $("#"+activeTab).fadeIn();		
-		
-      $("ul.tabs li").removeClass("active");
-      $(this).addClass("active");
+       $(".tab_content").hide();
+       $(".tab_container").each(function() {
+         $( this ).children(".tab_content").first().show();
+      });
+      $(".tabs").each(function() {
+        $( this ).children().first().addClass("active");
+     });
 
-	  $(".tab_drawer_heading").removeClass("d_active");
-	  $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
-	  
-    });
-	/* if in drawer mode */
-	$(".tab_drawer_heading").click(function() {
-      
-      $(".tab_content").hide();
-      var d_activeTab = $(this).attr("rel"); 
-      $("#"+d_activeTab).fadeIn();
-	  
-	  $(".tab_drawer_heading").removeClass("d_active");
-      $(this).addClass("d_active");
-	  
-	  $("ul.tabs li").removeClass("active");
-	  $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
-    });
-	
-	
-	/* Extra class "tab_last" 
-	   to add border to right side
-	   of last tab */
-	$('ul.tabs li').last().addClass("tab_last");
-	
+     /* if in tab mode */
+       $("ul.tabs li").click(function() {
+
+         var activeTab = $(this).attr("rel");
+          $("#"+activeTab).siblings('.tab_content').hide();
+         $("#"+activeTab).slideDown(200);
+
+         $(this).siblings().removeClass("active");
+         $(this).addClass("active");
+
+
+       });
+     /* if in drawer mode */
+     $(".tab_drawer_heading").click(function() {
+
+
+         var d_activeTab = $(this).attr("rel");
+         $("#"+d_activeTab).siblings('.tab_content').hide();
+         $("#"+d_activeTab).slideDown(200);
+
+         $(this).siblings(".tab_drawer_heading").removeClass("d_active");
+         $(this).addClass("d_active");
+
+       $("ul.tabs li").removeClass("active");
+       $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+       });
