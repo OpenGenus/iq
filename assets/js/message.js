ee6bcabbe08f16888336e697832962d2eaeb2fe2
David Xuan <davidxuanzc@gmail.com>
2019-03-17 13:23:32 +0800

    improve map UI, add message banner

---
+++ assets/js/message.js
@@ -0,0 +1,4 @@
+var msg = {
+    "text": "Want to contribute to Algorithms? See this task list and make your first contribution today 💪.",
+    "link": "https://discourse.opengenus.org/t/task-list-algorithms/2052/"
+}
