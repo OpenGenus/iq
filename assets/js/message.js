a21a3d973d99af4d8ee29dd83c933e0f18712f01
David Xuan <davidxuanzc@gmail.com>
2019-03-22 12:59:01 +0800

    generate global message by tags

--- assets/js/message.js
+++ assets/js/message.js
@@ -1,9 +1,23 @@
 var msg_contents = {
+    "message": [{
+        "text": "Did you know that you can contribute to OpenGenus IQ as well and become a writer 💪.",
+        "link": "https://discourse.opengenus.org/t/become-a-writer-at-opengenus-iq/843/",
+    }, {
+        "text": "Join our community today and ask us anything 💪.",
+        "link": "https://discourse.opengenus.org/"
+    }],
     "algorithm": [{
         "text": "Want to contribute to Algorithms? See this task list and make your first contribution today 💪.",
         "link": "https://discourse.opengenus.org/t/task-list-algorithms/2052/",
     }, {
         "text": "Want to contribute to Data Structure? See this task list and make your first contribution today 💪.",
         "link": "https://discourse.opengenus.org/t/task-list-data-structures/1898/"
+    }],
+    "data-structure": [{
+        "text": "See how Van Emde Boas tree performs operations in O(log log N) time complexity 💪.",
+        "link": "https://discourse.opengenus.org/t/van-emde-boas-tree/2216",
+    }, {
+        "text": "Learn about Y fast trie, the bitwise version of trie 💪.",
+        "link": "https://discourse.opengenus.org/t/y-fast-trie-data-structure/2152"
     }]
 }
