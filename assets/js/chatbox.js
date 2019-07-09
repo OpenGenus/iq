8d6eacd7aad2c994151d3c49968d74ecad008b82
David Xuan <davidxuanzc@gmail.com>
2019-07-09 17:26:13 +0800

    change sidebar to floating button

--- assets/js/chatbox.js
+++ assets/js/chatbox.js
@@ -1,30 +1,27 @@
+// Discourse forum  popup and the live support feature goes here
+var discourse_msg_active = false;
 
+$(document).ready(function () {
 
+  $('#Discourse_button').click(function () {
 
-    // Discourse forum  popup and the live support feature goes here
-var discourse_msg_active=false;
+    if (discourse_msg_active == false) {
 
-         $(document).ready(function(){
+      $('#Discourse_Forum').slideDown(300);
+      discourse_msg_active = true;
 
-             $('#Discourse_button').click(function(){
+    } else {
+      $('#Discourse_Forum').slideUp(300);
 
-               if(discourse_msg_active==false){
+      discourse_msg_active = false;
 
-                $('#Discourse_Forum').slideDown(300);
-                 discourse_msg_active=true;
+    }
 
-               }else{
-                    $('#Discourse_Forum').slideUp(300);
+  });
+  // Controls for the close button for this popup
+  $('#Discourse_close').click(function () {
+    $('#Discourse_Forum').slideUp(300);
+    discourse_msg_active = false;
 
-                 discourse_msg_active=false;
-
-               }
-
-            });
-            // Controls for the close button for this popup
-            $('#Discourse_close').click(function(){
-                $('#Discourse_Forum').slideUp(300);
-                discourse_msg_active=false;
-
-            });
-  })
+  });
+})
