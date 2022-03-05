111c32a49c51336b0123b67c33f64c6def612b7e
lumunge <lumungep12@gmail.com>
2022-03-05 20:36:40 +0300

    sticky left side-bar

--- assets/js/left-sidebar.js
+++ assets/js/left-sidebar.js
@@ -22,29 +22,29 @@ window.onscroll = function () {
         wrapper.getBoundingClientRect().bottom < window.innerHeight &&
         wrapper.getBoundingClientRect().bottom < sidebar.offsetHeight
       ) {
-        wrapper.classList.remove("fix-top-VP");
+        wrapper.classList.remove("fix-top");
         wrapper.classList.add("flex-bottom");
       } else if (wrapper.getBoundingClientRect().top < 0) {
-        wrapper.classList.add("fix-top-VP");
+        wrapper.classList.add("fix-top");
         wrapper.classList.remove("flex-bottom");
       } else {
-        wrapper.classList.remove("fix-top-VP");
+        wrapper.classList.remove("fix-top");
         wrapper.classList.remove("flex-bottom");
       }
     }
     //wrapper taller than sidebar - stick to bottom
     else if (sidebar.offsetHeight < wrapper.offsetHeight) {
       if (wrapper.getBoundingClientRect().bottom < window.innerHeight) {
-        wrapper.classList.remove("fix-bottom-VP");
+        wrapper.classList.remove("fix-bottom");
         wrapper.classList.add("flex-bottom");
       } else if (
         wrapper.getBoundingClientRect().bottom >
         sidebar.offsetHeight + window.innerHeight
       ) {
-        wrapper.classList.remove("fix-bottom-VP");
+        wrapper.classList.remove("fix-bottom");
         wrapper.classList.remove("flex-bottom");
       } else {
-        wrapper.classList.add("fix-bottom-VP");
+        wrapper.classList.add("fix-bottom");
         wrapper.classList.remove("flex-bottom");
       }
     }
